cdfcf8dbbcb0dedc5736cdb4227c80b6e748dd8e
rauzola <raul_sigoli@hotmail.com>
2020-07-05 00:53:27 -0300

    Tipos de Parâmetros

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 /**
  * Métodos HTTP:
  *
@@ -11,24 +13,41 @@ const app = express();
  * DELETE: Deletar uma informação no back-end
  */
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello World" });
+/**
+ * Tipos de parametros:
+ *
+ * Query Parmas: Filtros e paginação
+ * Route Params: Identificar recursos(Atualizar/Deletar)
+ * Request Body: Conteúdo na hora criar ou editar um recurso (JSON)
+ */
+
+app.get("/projects", (request, response) => {
+  const { title, owner } = request.query;
+
+  console.log(title);
+  console.log(owner);
+
+  return response.json(["Projeto 1", "Projeto 2"]);
 });
 
-app.get("/projects", (req, res) => {
-  return res.json(["Projeto 1", "Projeto 2"]);
+app.post("/projects", (request, response) => {
+  const body = request.body;
+
+  console.log(body);
+
+  return response.json(["Projeto 1", "Projeto 2", "Projeto 3"]);
 });
 
-app.post("/projects", (res, req) => {
-  return res.json(["Projeto 1", "Projeto 2", "Projeto 3"]);
+app.put("/projects/:id", (request, response) => {
+  const params = request.params;
+
+  console.log(params);
+
+  return response.json(["Projeto 4", "Projeto 2", "Projeto 3"]);
 });
 
-app.put("/projects/:id", (req, res) => {
-  return res.json(["Projeto 4", "Projeto 2", "Projeto 3"]);
-});
-
-app.delete("/projects/:id", (req, res) => {
-  return res.json(["Projeto 1", "Projeto 2", "Projeto 3"]);
+app.delete("/projects/:id", (request, response) => {
+  return response.json(["Projeto 1", "Projeto 2", "Projeto 3"]);
 });
 app.listen(3333, () => {
   console.log("🚀 Back-end started!");
